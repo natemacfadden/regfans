1d8f01066bf31adf939260d02fcd98f3e198bf53
Nate MacFadden <natemacfadden@gmail.com>
2026-01-08 15:04:49 -0500

    better deliminate sections

--- tutorials/simple_lifting_ex.py
+++ tutorials/simple_lifting_ex.py
@@ -15,6 +15,9 @@ heights = None            # picks a default Delaunay triangulation
 # ------------------------------
 vc  = VectorConfiguration(pts)
 print(f"constructed the following VC: {vc}")
+
+print()
+print('-'*60)
 print()
 
 # optional: check properties of the VC
@@ -23,19 +26,25 @@ print()
 print(f"is the VC solid? {vc.is_solid()}")
 print(f"is the VC totally cyclic? {vc.is_totally_cyclic()}")
 print(f"what are the bounding hyperplanes of the VC? {vc.support()}")
+
+print()
+print('-'*60)
 print()
 
 # construct/study a triangulation via lifting
 # -------------------------------------------
 # via lifting
 fan = vc.subdivide(heights=heights)
-print(f"constructed the following fan: {vc}")
+print(f"constructed the following fan: {fan}")
+
+print()
+print('-'*60)
 print()
 
 # read some properies of the fan
-print(f"the default triangulation has cones {fan.cones()}")
+print(f"the triangulation has cones {fan.cones()}")
 print(f"some heights defining the triangulation are {fan.heights().tolist()}")
-print(f"is the default triangulation valid? {fan.is_valid()}")
-print(f"is the default triangulation fine? {fan.is_fine()}")
-print(f"is the default triangulation regular? {fan.is_regular()}")
-print(f"does the default triangulation define a triangulation of the associated PC? {fan.respects_ptconfig()}")
+print(f"is the triangulation valid? {fan.is_valid()}")
+print(f"is the triangulation fine? {fan.is_fine()}")
+print(f"is the triangulation regular? {fan.is_regular()}")
+print(f"does the triangulation define a triangulation of the associated PC? {fan.respects_ptconfig()}")
